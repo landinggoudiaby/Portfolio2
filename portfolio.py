fd71359fc479f0f69aef0b9dad2651a031aba27a
landinggoudiaby <landinggoudiaby2032@gmail.com>
2026-02-16 21:41:25 +0000

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+with st.sidebar:
+    st.title("INFORMATIONS")
+    st.image("lg.jpeg")
+    st.text("Landing GOUDIABY")
+    st.text("Tel: 76 302 47 10")
+    st.text("email: landinggoudiaby2032@gmail.com")
+
+
+    st.title("EDUCATIONS")
+    st.markdown("""
+    
+    * BTS Geomatique
+
+    *BAC L2:2024-2025
+    """)
+
+st.title("Profile")
+
+st.subheader("Technicien supérieur en géomatique")
+
+
+
+st.title("Projet Academique")
+st.markdown("""
+
+*Réalisations des cartes thématiques sur QGis et ArcGis
+
+*Collecte de données au massalikul,G15et de colobane,Fass
+
+*Numérisation d'une base de données
+
+""")
+
+st.title("Expériences")
+st.markdown("""
+*ouvrier à l'usine de Gandour:2024-2025
+""")
+
+st.title("Projets personnels")
+st.markdown("""
+
+""")
